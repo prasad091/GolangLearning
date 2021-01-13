3444ad3aa1922fdbb5359765bdcd3c837f47ee27
prasad091 <prasad.thangavel@gmail.com>
2021-01-13 11:19:11 +0530

    inheritence in go (#3)

---
+++ inhertitance/example.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Example struct {
+	data        string
+	anotherdata int
+}
+
+func (v *Example) getData() string {
+	return v.data
+}
+
+func (v *Example) getAnotherData() int {
+	return v.anotherdata * 5
+}
+
+type ExampleTwo struct {
+	Example
+}
+
+type ExampleThree struct {
+	Base Example
+}
+
+func main() {
+	exampleTwo := &ExampleTwo{
+		Example{"Prasad", 5},
+	}
+	fmt.Println(exampleTwo.getData())
+}
