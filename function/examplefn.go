e028ba8d4ecf6b545158d6c9d4ccc63d9af3a129
prasad091 <prasad.thangavel@gmail.com>
2021-01-29 10:05:37 +0530

    Function in go

---
+++ function/examplefn.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func add(x int, y int) int {
+	var out = x + y
+	return out
+}
+
+func main() {
+	num1 := 5
+	num2 := 4
+
+	result := add(num1, num2)
+	fmt.Println(result)
+}
